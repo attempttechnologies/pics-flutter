6c404b0efe2f519d1eac9e2db45720babcc2b3ab
Sulejman Limaj <limaj.sulejman@gmail.com>
2020-05-31 05:10:14 +0200

    stateless widget to stateful widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('${this.counter}'),
         appBar: AppBar(
           title: Text('Let\'s See Some Images!'),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() => this.counter++);
+          },
           child: Icon(
             Icons.add,
           ),
